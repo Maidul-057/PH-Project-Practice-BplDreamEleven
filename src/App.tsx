d5c341aee584d67cec1989bb72fbe1dd11574380
Maidul Islam <maidulislamzishan@gmail.com>
2026-09-11 15:33:42 +0600

    updated the file

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import Banner from "./components/Banner"
 import Nav from "./components/Nav"
+import Players from "./components/players/players";
 
 function App() {
 
      const playerPromise=async()=>{
       const res = await fetch('/data.json')
+      const data = await res.json()
+      return data;
+     };
 
   return (
     <> 
     <Nav />
     <Banner />
+    <Players playerPromise={playerPromise}/>
       </>
   )
 }
---
+++ src/components/players/Players.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const players = ({ playersPromise }) => {
+    return (
+        <div>
+            console.log(playersPromise);
+        </div>
+    );
+};
+
+export default players;
