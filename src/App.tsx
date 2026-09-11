61b35a22a754f490d7a872900abd607ccff41965
Maidul Islam <maidulislamzishan@gmail.com>
2026-09-11 19:32:36 +0600

    updated the file

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { Suspense } from "react";
 import Banner from "./components/Banner"
 import Nav from "./components/Nav"
 import Players from "./components/players/Players";
+import type { Iplayer } from "./types/plaerType";
 
- const playerPromiseFetch=async()=>{
+ const playerPromiseFetch=async():Promise<Iplayer[]>=>{
       const res = await fetch('/data.json')
       const data = await res.json()
       return data;}
@@ -19,6 +20,7 @@ function App() {
     <Suspense fallback={<div>Loading...</div>}>     
     <Players playersPromise={playersPromise}/>
       </Suspense>
+      </>
   )
 }
 
--- src/components/players/Players.tsx
+++ src/components/players/Players.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { use } from 'react';
+import type { Iplayer } from '../../types/plaerType';
+interface playerProps{ 
+    playersPromise:Promise<Iplayer[]>
+}
 
-
-const Players = ({ playersPromise }) => {
+const Players = ({ playersPromise }:playerProps) => {
      console.log(playersPromise);
      const players = use(playersPromise);
      console.log(players, "Players");
-    return 
+    return (
         <div>
-           
+           console.log(players, "Players");
         </div>
-    ;
+    );
 };
 
 export default Players;
