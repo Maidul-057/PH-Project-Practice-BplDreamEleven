7d21ef47befa733ad3c270459feba6646fbbcb78
Maidul Islam <maidulislamzishan@gmail.com>
2026-09-11 12:31:45 +0600

    updated all files

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <> 
     <nav className="flex justify-between" > 
       <img src={logo} alt="logo" />
-      <ul className="flex gap-4 items-center">
+      <ul className="flex gap-5 items-center">
         <li>Home</li>
         <li>Fixtures</li>
         <li>Player</li>
